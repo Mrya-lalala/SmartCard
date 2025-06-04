38e03e335ee183040a148864f9161034f7ffb594
Mourya BS <mouryabs@Mouryas-MacBook-Air.local>
2025-06-04 11:32:50 +0530

    Add root / route for Render

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def home():
+def api_home():
     return jsonify({
         "message": "Smart Card API is live!",
         "endpoints": [
